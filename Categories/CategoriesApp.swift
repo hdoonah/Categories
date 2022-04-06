afe6232425cb70fa63455df8bc13644bbb52a0a5
hdoonah <Huda.alaslami@gmail.com>
2022-04-06 19:53:40 +0300

    Initial Commit

---
+++ Categories/CategoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CategoriesApp.swift
+//  Categories
+//
+//  Created by HUDA ALSHAMMARI on 05/09/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct CategoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Categories/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Categories
+//
+//  Created by HUDA ALSHAMMARI on 05/09/1443 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
